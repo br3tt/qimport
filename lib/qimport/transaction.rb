defe17dfd02292c51c640d0cf57dcb6956b8f5be
Brett Basler <geekbrett@gmail.com>
2013-09-11 23:16:36 -0700

    remove default columns for transactions

--- lib/qimport/transaction.rb
+++ lib/qimport/transaction.rb
@@ -1,94 +1,14 @@
 class Qimport::Transaction
   attr_accessor :data, :distribution_lines
 
-  DEFAULT_DATA = {
-    'BILL' => {
-      trnsid:'',
-      trnstype: '',
-      date: '',
-      accnt: '',
-      name: '',
-      class: '',
-      amount: '',
-      docnum: '',
-      memo: '',
-      clear: '',
-      toprint: '',
-      addr5: '',
-      duedate: '',
-      terms: ''
-    },
-    'INVOICE' => {
-      trnsid:'',
-      trnstype: '',
-      date: '',
-      accnt: '',
-      name: '',
-      class: '',
-      amount: '',
-      docnum: '',
-      memo: '',
-      clear: '',
-      toprint: '',
-      addr1: '',
-      addr2: '',
-      addr3: '',
-      addr4: '',
-      addr5: '',
-      duedate: '',
-      terms: '',
-      paid: '',
-      shipdate: ''
-    }
-  }
-
-  DEFAULT_LINE_DATA = {
-    'BILL' => {
-      splid: '',
-      trnstype: '',
-      date: '',
-      accnt: '',
-      name: '',
-      class: '',
-      amount: '',
-      docnum: '',
-      memo: '',
-      clear: '',
-      qnty: '',
-      reimbexp: '',
-      servicedate: '',
-      other2: ''
-    },
-    'INVOICE' => {
-      splid: '',
-      trnstype: '',
-      date: '',
-      accnt: '',
-      name: '',
-      class: '',
-      amount: '',
-      docnum: '',
-      memo: '',
-      clear: '',
-      qnty: '',
-      price: '',
-      invitem: '',
-      paymeth: '',
-      taxable: '',
-      reimbexp: '',
-      extra: '',
-      servicedate: ''
-    },
-  }
-
   def initialize(data)
-    @data = DEFAULT_DATA[data[:trnstype]].merge(data)
+    @data = data
 
     @distribution_lines = []
   end
 
   def line(data)
-    data = DEFAULT_LINE_DATA[data[:trnstype]].merge(data)
+    data = data
 
     @distribution_lines << data
   end
